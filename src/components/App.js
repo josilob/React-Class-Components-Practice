4d0c376e0a44a250013ac5db85c650fd427fbc8f
josilob <bjosilo@gmail.com>
2021-02-03 22:47:53 -0500

    conditionally rendered component

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,31 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from '../components/VideoList';
+import VideoDetail from '../components/VideoDetail';
 
 class App extends React.Component {
-	state = { videos: [] };
+	state = { videos: [], selectedVideo: null };
+
 	handleTermSubmit = async (term) => {
 		const res = await youtube.get('/search', { params: { q: term } });
 		this.setState({ videos: res.data.items });
-		console.log(res.data.items);
+		// console.log(res.data.items);
+	};
+
+	onVideoSelect = (video) => {
+		console.log('clg from the app', video);
+		this.setState({ selectedVideo: video });
 	};
 
 	render() {
 		return (
 			<div className='ui container'>
 				<SearchBar onFormSubmit={this.handleTermSubmit} />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					videos={this.state.videos}
+					onVideoSelect={this.onVideoSelect}
+				/>
 			</div>
 		);
 	}
