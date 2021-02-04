3e32ec731798d9a63c5532301a513bdf5ded9d9c
josilob <bjosilo@gmail.com>
2021-02-03 22:26:08 -0500

    fetched videos, displayed and thumbnails

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from '../components/VideoList';
 
 class App extends React.Component {
 	state = { videos: [] };
 	handleTermSubmit = async (term) => {
 		const res = await youtube.get('/search', { params: { q: term } });
 		this.setState({ videos: res.data.items });
+		console.log(res.data.items);
 	};
 
 	render() {
 		return (
 			<div className='ui container'>
-				<SearchBar onFormSubmit={this.handleTermSubmit} />I have{' '}
-				{this.state.videos.length} videos
+				<SearchBar onFormSubmit={this.handleTermSubmit} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
---
+++ src/components/VideoItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const VideoItem = ({ video }) => {
+	return (
+		<div>
+			{video.snippet.title}
+			<img src={video.snippet.thumbnails.medium.url} />
+		</div>
+	);
+};
+
+export default VideoItem;
+//video.snippet.title
+//
